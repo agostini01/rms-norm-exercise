b486b297ff556fcb9124f0da59e504991e3bd374
Nicolas Agostini <n.b.agostini@gmail.com>
2024-04-21 18:00:10 -0400

    Implement baseline in triton
    Implements in custom Triton kernels
    - square function

--- tests/test_rmsnorm.py
+++ tests/test_rmsnorm.py
@@ -1,6 +1,7 @@
 import torch
 import unittest
 from baselines.rmsnorm import MyRMSNorm, RMSNormGT1, RMSNormL3
+from optimized.rmsnorm import TritonRMSNorm
 
 class TestRMSNorm(unittest.TestCase):
     def test_forward_my(self):
@@ -23,6 +24,20 @@ class TestRMSNorm(unittest.TestCase):
         
         # Compute the expected output
         rmsnorm_test = RMSNormGT1(dim=3)
+        expected_output = rmsnorm_test.forward(x)
+        
+        # Compute the actual output
+        rmsnorm_correct = RMSNormL3(dim=3)
+        actual_output = rmsnorm_correct.forward(x)
+        
+        self.assertTrue(torch.all(torch.eq(actual_output, expected_output)))
+
+    def test_forward_triton(self):
+        
+        x = torch.tensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+        
+        # Compute the expected output
+        rmsnorm_test = TritonRMSNorm(dim=3)
         expected_output = rmsnorm_test.forward(x)
         
         # Compute the actual output
