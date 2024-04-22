587aff1ce6c1bdca6d220708dc7adb31e0ec52ac
Nicolas Agostini <n.b.agostini@gmail.com>
2024-04-22 07:23:41 +0000

    Update tests to include float32 and float16 tests
    - Before this commit, all tests where using int64

--- baselines/rmsnorm.py
+++ baselines/rmsnorm.py
@@ -47,3 +47,17 @@ class RMSNormL3(torch.nn.Module):
 	def forward(self, x):
 		output = self._norm(x.float()).type_as(x)
 		return output * self.weight	
+
+class RMSNormL3Half(torch.nn.Module):
+	"""Implementation extracted from LLAMA3."""
+	def __init__(self, dim: int, eps: torch.float16 = 1e-6):
+		super().__init__()
+		self.eps = eps
+		self.weight = nn.Parameter(torch.ones(dim))
+
+	def _norm(self, x):
+		return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+	def forward(self, x):
+		output = self._norm(x.half()).type_as(x)
+		return output * self.weight	
--- tests/test_rmsnorm.py
+++ tests/test_rmsnorm.py
@@ -1,6 +1,6 @@
 import torch
 import unittest
-from baselines.rmsnorm import MyRMSNorm, RMSNormGT1, RMSNormL3
+from baselines.rmsnorm import MyRMSNorm, RMSNormGT1, RMSNormL3, RMSNormL3Half
 from optimized.rmsnorm import RMSNormTriton
 
 class TestRMSNorm(unittest.TestCase):
@@ -34,7 +34,9 @@ class TestRMSNorm(unittest.TestCase):
 
     def test_forward_triton(self):
         
-        x = torch.tensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+        x = torch.tensor([[[1, 2, 3], [5, 5, 5]], [[7, 8, 9], [10, 11, 12]]])
+        # Change all to float
+        x = x.float()
         
         # Compute the actual output
         rmsnorm_correct = RMSNormL3(dim=3)
@@ -44,7 +46,25 @@ class TestRMSNorm(unittest.TestCase):
         rmsnorm_test = RMSNormTriton(dim=3).cuda()
         expected_output = rmsnorm_test.forward(x.to('cuda:0'))
         
-        self.assertTrue(torch.all(torch.eq(actual_output, expected_output.to('cpu'))))
+        self.assertTrue(torch.allclose(actual_output, expected_output.to('cpu')))
+    
+    def test_forward_triton_half(self):
+        
+        x = torch.tensor([[[1, 2, 3], [5, 5, 5]], [[7, 8, 9], [10, 11, 12]]])
+        # Change all to float
+        x = x.half()
+        
+        # Compute the actual output
+        rmsnorm_correct = RMSNormL3Half(dim=3).half()
+        actual_output = rmsnorm_correct.forward(x)
+        
+        # Compute the output
+        rmsnorm_test = RMSNormTriton(dim=3).cuda()
+        expected_output = rmsnorm_test.forward(x.to('cuda:0'))
+
+        # TODO: It appears that operating in half precision on CPU and GPU is not the same as in float precision 
+        #       Absolute tolerance was relaxed from 1e-8 to 1e-3
+        self.assertTrue(torch.allclose(actual_output.to('cpu'), expected_output.to('cpu'), atol=1e-3))
 
 if __name__ == '__main__':
     unittest.main()
