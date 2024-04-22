c337e000b1724d851a95ffe062ca1e519b590ca5
Nicolas Agostini <n.b.agostini@gmail.com>
2024-04-22 03:00:20 +0000

    Update triton test to run on `cuda:0`

--- tests/test_rmsnorm.py
+++ tests/test_rmsnorm.py
@@ -1,14 +1,14 @@
 import torch
 import unittest
 from baselines.rmsnorm import MyRMSNorm, RMSNormGT1, RMSNormL3
-from optimized.rmsnorm import TritonRMSNorm
+from optimized.rmsnorm import RMSNormTriton
 
 class TestRMSNorm(unittest.TestCase):
     def test_forward_my(self):
         
         x = torch.tensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
         
-        # Compute the expected output
+        # Compute the output
         rmsnorm_test = MyRMSNorm(dim=3)
         expected_output = rmsnorm_test.forward(x)
         
@@ -22,7 +22,7 @@ class TestRMSNorm(unittest.TestCase):
         
         x = torch.tensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
         
-        # Compute the expected output
+        # Compute the output
         rmsnorm_test = RMSNormGT1(dim=3)
         expected_output = rmsnorm_test.forward(x)
         
@@ -36,15 +36,15 @@ class TestRMSNorm(unittest.TestCase):
         
         x = torch.tensor([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
         
-        # Compute the expected output
-        rmsnorm_test = TritonRMSNorm(dim=3)
-        expected_output = rmsnorm_test.forward(x)
-        
         # Compute the actual output
         rmsnorm_correct = RMSNormL3(dim=3)
         actual_output = rmsnorm_correct.forward(x)
         
-        self.assertTrue(torch.all(torch.eq(actual_output, expected_output)))
+        # Compute the output
+        rmsnorm_test = RMSNormTriton(dim=3).cuda()
+        expected_output = rmsnorm_test.forward(x.to('cuda:0'))
+        
+        self.assertTrue(torch.all(torch.eq(actual_output, expected_output.to('cpu'))))
 
 if __name__ == '__main__':
     unittest.main()
